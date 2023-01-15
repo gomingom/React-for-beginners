da040514baf3ed0b82133ffdb08948e5a5d1dacf
YH Kim <yeongh.kim17@gmail.com>
2023-01-15 16:07:01 +0900

    Create ToDos as component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,11 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import ToDos from "./ToDos";
+
 import { useState } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => {
-    setToDo(event.target.value);
-  };
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((currentArray) => [toDo, ...currentArray]);
-    setToDo("");
-  };
   return (
     <div>
-      <h1> My To Dos: {toDos.length}</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={toDo}
-          placeholder="Write You To Do..."
-        ></input>
-        <button>Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, index) => (
-          <li key={index}>{item}</li>
-        ))}
-      </ul>
+      <ToDos />
     </div>
   );
 }
---
+++ src/ToDos.js
@@ -0,0 +1,36 @@
+import { useState, useEffect } from "react";
+function ToDos() {
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => {
+    setToDo(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((currentArray) => [toDo, ...currentArray]);
+    setToDo("");
+  };
+  return (
+    <div>
+      <h1> My To Dos: {toDos.length}</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          placeholder="Write You To Do..."
+        ></input>
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+export default ToDos;
